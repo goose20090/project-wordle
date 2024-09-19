3fd928754cbc079bfd55f16aef73de30e14b85fe
Angus Bower-Brown <angus.bower-brown@dentally.co>
2024-09-19 22:45:39 +0100

    Conditional Rendering for the Banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
-function Banner() {
-  return <div></div>;
+function Banner({ happy, guessNum }) {
+  let bannerMood = happy ? 'happy' : 'sad';
+  return (
+    <div className={`${bannerMood} banner`}>
+      <p>
+        {happy ? (
+          <>
+            {' '}
+            <strong>Congratulations!</strong> Got it in{' '}
+            <strong>{guessNum} guesses</strong>.
+          </>
+        ) : (
+          <>
+            Sorry, the correct answer is <strong>LEARN</strong>.
+          </>
+        )}
+      </p>
+    </div>
+  );
 }
 
 export default Banner;
