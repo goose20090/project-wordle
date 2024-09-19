5ed263d990bae7964bd34580ddb7651ce9433f44
Angus Bower-Brown <angus.bower-brown@dentally.co>
2024-09-19 20:12:30 +0100

    Control guess input component

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,14 +1,23 @@
 import React from 'react';
 
 function GuessInput() {
+  const [guess, setGuess] = React.useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(guess);
+  }
   return (
-    <form class="guess-input-wrapper">
+    <form class="guess-input-wrapper" onSubmit={handleSubmit}>
       <label for="guess-input">
         Enter guess:
         <input
           id="guess-input"
           pattern={'[A-Z]{5}'}
           title="Your guess must be 5 characters long. Only letters allowed!"
+          value={guess}
+          onChange={e => setGuess(e.target.value.toUpperCase())}
+        ></input>
       </label>
     </form>
   );
