9fa2fc53a01255b69b409546e3850ff1ba10f891
Angus Bower-Brown <angus.bower-brown@dentally.co>
2024-09-19 21:40:51 +0100

    Initialise guesses state with starting grid and guessNum, import components

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -4,7 +4,9 @@ import { range, sample } from '../../utils';
 import { WORDS } from '../../data';
 import GuessInput from '../GuessInput/GuessInput';
 import GuessList from '../GuessList/GuessList';
+import GuessesGrid from '../GuessesGrid/GuessesGrid';
 import { EMPTY_GUESS, NUM_OF_GUESSES_ALLOWED } from '../../constants';
+import Guess from '../Guess/Guess';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -12,10 +14,13 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
-  const [guesses, setGuesses] = React.useState([]);
   const startingGrid = range(NUM_OF_GUESSES_ALLOWED).map(
     () => EMPTY_GUESS
   );
+
+  const [guesses, setGuesses] = React.useState(startingGrid);
+  const [guessNum, setGuessNum] = React.useState(0);
+
   return (
     <>
       <GuessInput guesses={guesses} setGuesses={setGuesses} />
--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -3,6 +3,11 @@ import React from 'react';
 function Guess({ guess }) {
   return (
     <p className="guess">
+      {guess.split('').map(char => (
+        <span className="cell" key={crypto.randomUUID()}>
+          {char}
+        </span>
+      ))}
     </p>
   );
 }
