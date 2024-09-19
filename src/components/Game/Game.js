bd79109435787b6ee8c76e09c31eba9481f9361a
Angus Bower-Brown <angus.bower-brown@dentally.co>
2024-09-19 21:38:00 +0100

    constant for empty guess; range utility and num of guesses constant to create startingGrid

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-import { sample } from '../../utils';
+import { range, sample } from '../../utils';
 import { WORDS } from '../../data';
 import GuessInput from '../GuessInput/GuessInput';
 import GuessList from '../GuessList/GuessList';
+import { EMPTY_GUESS, NUM_OF_GUESSES_ALLOWED } from '../../constants';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -12,6 +13,9 @@ console.info({ answer });
 
 function Game() {
   const [guesses, setGuesses] = React.useState([]);
+  const startingGrid = range(NUM_OF_GUESSES_ALLOWED).map(
+    () => EMPTY_GUESS
+  );
   return (
     <>
       <GuessInput guesses={guesses} setGuesses={setGuesses} />
